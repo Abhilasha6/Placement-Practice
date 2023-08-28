99e0b2f967fe44ee26230dec7020cfb5f2df8f75
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-08-28 23:29:58 +0530

    o(n) optimised solution to find the max rectangle area possible

--- medium level/container_with_most_water.java
+++ medium level/container_with_most_water.java
@@ -1,16 +1,23 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n=height.length;
-        if(n==2) return Math.min(height[0], height[1]);
-        int area=0, max=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                area=(j-i)*height[j];
-                if(area > max && height[j]<=height[i])
-                max=area;
+        int n = height.length;
+        if (n == 2)
+            return Math.min(height[0], height[1]);
+        int area = 0, max = 0;
+        for (int i = 0, j = n - 1; i < j;) {
+            // for(int j=i+1;j<n-1;j++)
+            // {
+            area = (j - i) * Math.min(height[j], height[i]);
+            max = area > max ? area : max;
+            if (height[j] < height[i])
+                j--;
+            else if (height[j] > height[i])
+                i++;
+            else {
+                i++;
+                j--;
             }
+            // }
         }
         return max;
     }

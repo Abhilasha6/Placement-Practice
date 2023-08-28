c70bbcd929a483b3fb1e3de53cefd31b2c109d86
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-08-29 00:00:45 +0530

    find no of pairs whose sum = k, use binary search for optimised code

--- medium level/max_no_of_k_sum_pairs.java
+++ medium level/max_no_of_k_sum_pairs.java
@@ -1,14 +1,38 @@
+import java.util.Arrays;
+
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int max = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == k && nums[i] != -1 && nums[j] != -1) {
-                    nums[i] = -1;
-                    nums[j] = -1;
-                    max++;
-                }
-            }
+
+        // TLE
+
+        // for(int i=0;i<nums.length-1;i++)
+        // {
+        // for(int j=i+1;j<nums.length;j++)
+        // {
+        // if(nums[i]+nums[j]==k && nums[i]!=-1 && nums[j]!=-1)
+        // {
+        // nums[i]=-1;
+        // nums[j]=-1;
+        // max++;
+        // }
+        // }
+        // }
+
+        // using binary searh algo & 2 pointers
+        Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == k && nums[i] != -1 && nums[j] != -1) {
+                nums[i] = -1;
+                nums[j] = -1;
+                max++;
+                i++;
+                j--;
+            } else if (nums[i] + nums[j] < k) {
+                i++;
+            } else
+                j--;
         }
         return max;
     }

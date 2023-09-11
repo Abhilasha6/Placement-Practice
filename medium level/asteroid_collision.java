0e263d6ce83a4ba045682b4bfdcabbd4dc1734e4
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-09-12 00:17:06 +0530

    if nos are same, pop both; if one is + and one is - then keep the biggest value; else push no.

--- medium level/asteroid_collision.java
+++ medium level/asteroid_collision.java
@@ -1,27 +1,34 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int n = asteroids.length;
-        Stack<Integer> s = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            if (asteroids[i] > 0 || s.isEmpty()) {
-                s.push(asteroids[i]);
-            } else {
-                while (!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(asteroids[i])) {
-                    s.pop();
+        Stack<Integer> st = new Stack<>();
+        // st.push(asteroids[0]);
+        for (int i = 0; i < asteroids.length; i++) {
+            // for(int count=1;count<=i;count++)
+            // if((asteroids[i]<0 && asteroids[i-1]>0)||(asteroids[i-1]<0 &&
+            // asteroids[i]>0))
+            // {
+            // st.pop();
+            // if(Math.abs(asteroids[i])!=Math.abs(asteroids[i-1]))
+            // st.push(Math.max(asteroids[i], asteroids[i-1]));
+            // else
+            // st.pop();
+            // }
+            // else
+            // st.push(asteroids[i]);
+
+            if (!st.isEmpty() && st.peek() > 0 && asteroids[i] < 0) {
+                if (Math.abs(asteroids[i]) == Math.abs(st.peek()))
+                    st.pop();
+                else if (Math.abs(asteroids[i]) > Math.abs(st.peek())) {
+                    st.pop();
+                    i--;
                 }
-                if (!s.isEmpty() && s.peek() == Math.abs(asteroids[i])) {
-                    s.pop();
-                } else {
-                    if (s.isEmpty() || s.peek() < 0) {
-                        s.push(asteroids[i]);
-                    }
-                }
-            }
+            } else
+                st.push(asteroids[i]);
         }
-        int[] res = new int[s.size()];
-        for (int i = s.size() - 1; i >= 0; i--) {
-            res[i] = s.pop();
-        }
+        int res[] = new int[st.size()];
+        for (int i = 0; i < res.length; i++)
+            res[i] = st.get(i);
         return res;
     }
 }

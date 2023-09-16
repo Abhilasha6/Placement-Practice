8c30c5b59b55e9fb515b3118531ca95e1045a04f
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-09-16 21:05:50 +0530

    return the count of  no. of days you have to wait after the ith day to get a warmer temperature

--- medium level/daily_temperatures.java
+++ medium level/daily_temperatures.java
@@ -1,10 +1,20 @@
+import java.util.Stack;
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int ans[] = new int[temperatures.length];
-        for (int i = 0; i < temperatures.length - 1; i++) {
-            while (i < temperatures.length - 1 && temperatures[i + 1] < temperatures[i])
-                ans[i + 1] += 1;
+        // correct but has TLE
+        // for (int i = 0; i < temperatures.length - 1; i++) {
+        // while (i < temperatures.length - 1 && temperatures[i + 1] < temperatures[i])
+        // ans[i + 1] += 1;
 
+        // optimised solution
+        Stack<Integer> st = new Stack<Integer>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!st.empty() && temperatures[st.peek()] < temperatures[i])
+                ans[st.peek()] = i - st.pop();
+
+            st.push(i);
         }
         ans[temperatures.length - 1] = 0;
         return ans;

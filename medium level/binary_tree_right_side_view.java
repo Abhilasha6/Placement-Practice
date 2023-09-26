2462ed513a2c31d3b498b1c8761d645b857d3c00
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-09-26 21:54:45 +0530

    return the right side view of a tree (can be unbalanced)

--- medium level/binary_tree_right_side_view.java
+++ medium level/binary_tree_right_side_view.java
@@ -1,37 +1,59 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  * int val;
+//  * TreeNode left;
+//  * TreeNode right;
+//  * TreeNode() {}
+//  * TreeNode(int val) { this.val = val; }
+//  * TreeNode(int val, TreeNode left, TreeNode right) {
+//  * this.val = val;
+//  * this.left = left;
+//  * this.right = right;
+//  * }
+//  * }
+//  */
 
 // works only for balanced tree
+// class Solution {
+// int c = 0;
+// List<Integer> arr = new ArrayList<>();
+
+// public List<Integer> rightSideView(TreeNode root) {
+// if (root == null)
+// return arr;
+// if (c == 0)
+// arr.add(root.val);
+// if (root.right != null) {
+// arr.add(root.right.val);
+// c++;
+// rightSideView(root.right);
+// }
+// return arr;
+// }
+// }
+
+// for unbalanced tree
 class Solution {
-    int c = 0;
-    List<Integer> arr = new ArrayList<>();
+    void dfs(TreeNode node, List<Integer> arr, int depth) {
+        if (node == null)
+            return;
+        if (depth == arr.size())
+            arr.add(node.val);
+        dfs(node.right, arr, depth + 1);
+        dfs(node.left, arr, depth + 1);
+    }
 
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> arr = new ArrayList<>();
         if (root == null)
             return arr;
-        if (c == 0)
-            arr.add(root.val);
-        if (root.right != null) {
-            arr.add(root.right.val);
-            c++;
-            rightSideView(root.right);
-        }
-        // if(root.left!=null)
-        // rightSideView(root.left);
-        // arr.add(root.val);
+        dfs(root, arr, 0);
         return arr;
     }
 }

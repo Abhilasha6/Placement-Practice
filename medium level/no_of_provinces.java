1809ee6cd0410d39bb7349178dfd44ecc7247987
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-10-04 00:53:47 +0530

    my approach

---
+++ medium level/no_of_provinces.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int c = 0; // assume all cities are connected
+        for (int i = 0; i < connected.length; i++) {
+            for (int j = 0; j < connected[i].length; j++) {
+                if (isConnected[i][j] == 1 && isConnected[i][j + 1] == 1)
+                    c++;
+            }
+        }
+    }
+}

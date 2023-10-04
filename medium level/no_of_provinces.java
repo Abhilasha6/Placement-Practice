3f9e4bb0eb8c0a1c6d6ee2989744bd96918ca364
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-10-04 23:35:17 +0530

    find number of unconnected provinces

--- medium level/no_of_provinces.java
+++ medium level/no_of_provinces.java
@@ -1,10 +1,25 @@
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int c = 0; // assume all cities are connected
+//         for (int i = 0; i < connected.length; i++) {
+//             for (int j = 0; j < connected[i].length; j++) {
+//                 if (isConnected[i][j] == 1 && isConnected[i][j + 1] == 1)
+//                     c++;
+//             }
+//         }
+//     }
+// }
+
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int c = 0; // assume all cities are connected
-        for (int i = 0; i < connected.length; i++) {
-            for (int j = 0; j < connected[i].length; j++) {
-                if (isConnected[i][j] == 1 && isConnected[i][j + 1] == 1)
-                    c++;
+        int c=0; //assume all cities are connected
+        for(int i=0;i<connected.length;i++)
+        {
+            for(int j=0;j<connected[i].length;j++)
+            {
+                if(isConnected[i][j]==1 && isConnected[i][j+1]==1)
+                c++;
             }
         }
     }

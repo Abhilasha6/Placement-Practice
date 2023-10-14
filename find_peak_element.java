6903a8e3c36397e3a0497d3edd2df0fd8f32aacf
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-10-14 23:05:43 +0530

    element who is greater than both its neighbours = peak no.

---
+++ find_peak_element.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+
+        int i=1, j=nums.length-2;
+
+        while(i<=j)
+        {
+            int mid = i+(j-i)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+            return mid;
+            else if(nums[mid]<nums[mid-1])
+            j=mid-1;
+            else if(nums[mid]<nums[mid+1])
+            i=mid+1;
+        }
+        return -1;
+    }
+}find_peak_element{
+
+}

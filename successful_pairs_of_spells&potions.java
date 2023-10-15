1bd0ca72d747ea97b80930ba88c065800e343e7a
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-10-15 23:45:49 +0530

    optimised sol. using binary search

--- successful_pairs_of_spells&potions.java
+++ successful_pairs_of_spells&potions.java
@@ -1,14 +1,35 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int[] ans = new int[spells.length];
-        int c = 0;
+
+        // correct for most cases
+        // int c = 0;
+        // for (int i = 0; i < spells.length; i++) {
+        // c = 0;
+        // for (int j = 0; j < potions.length; j++) {
+        // if (spells[i] * potions[j] >= success)
+        // c++;
+        // }
+        // ans[i] = c;
+        // }
+        // return ans;
+
+        // search for the position where the 1st no. > success occurs,
+        // then subtract that index from total length to get no.s > success from the
+        // sorted potions array
+
+        Arrays.sort(potions);
         for (int i = 0; i < spells.length; i++) {
-            c = 0;
-            for (int j = 0; j < potions.length; j++) {
-                if (spells[i] * potions[j] >= success)
-                    c++;
+            int high = potions.length - 1, low = 0;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                long val = (long) spells[i] * potions[mid];
+                if (val >= success)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
             }
-            ans[i] = c;
+            ans[i] = potions.length - low;
         }
         return ans;
     }
